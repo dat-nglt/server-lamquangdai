7c4d38bd7c19b0dbfa3d607cfb82f6704656c4ed
Darren <nguyenletandat.contact@gmail.com>
2025-11-05 08:41:49 +0700

    POST - webhook

--- src/routes/zalo.webhook.route.js
+++ src/routes/zalo.webhook.route.js
@@ -4,5 +4,6 @@ import { webhookController } from "../controllers/webhook.controller.js";
 const routeForWebhook = express.Router();
 
 routeForWebhook.get("/", webhookController);
+routeForWebhook.post("/", webhookController);
 
 export default routeForWebhook;
