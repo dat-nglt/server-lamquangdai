e2d167062bde6bcf8696c7776e671a8f6a5e0d48
Darren <nguyenletandat.contact@gmail.com>
2025-11-10 10:25:28 +0700

    config webhook

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,17 +1,11 @@
 export const webhookController = async (req, res) => {
-  const data = req.body;
   res.status(200).send("EVENT_RECEIVED");
-  processData(data);
-  return;
-};
-
-const processData = (data) => {
-  console.log("Đã nhận sự kiện từ Zalo:", data.event_name);
-  // Ví dụ: Nếu là tin nhắn text
+  const data = req.body;
   if (data.event_name === "user_send_text") {
     const message = data.message.text;
     const sender_id = data.sender.id;
 
     console.log(message, sender_id);
   }
+  return;
 };
