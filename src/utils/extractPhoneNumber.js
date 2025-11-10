1a8237585c22a62a948bab0ebe72bddf58217fe2
Darren <nguyenletandat.contact@gmail.com>
2025-11-10 17:16:54 +0700

    Extract phone number

--- src/utils/extractPhoneNumber.js
+++ src/utils/extractPhoneNumber.js
@@ -1,20 +1,19 @@
 export const extractPhoneNumber = (messageFromUser) => {
   // Kiểm tra nếu đầu vào không phải là chuỗi
-  if (typeof tinNhan !== "string" || tinNhan.length === 0) {
-    return [];
+  if (typeof messageFromUser !== "string" || messageFromUser.length === 0) {
+    return 1;
   }
 
   // Khuôn mẫu Regex (tìm SĐT di động 10 số, bắt đầu bằng 0, +84, hoặc 84)
   const phoneRegex = /\b((0|\+84|84)[35789]\d{8})\b/g;
 
   // Thực hiện trích xuất
-  const ketQua = tinNhan.match(phoneRegex);
-
+  const ketQua = messageFromUser.match(phoneRegex);
   // Hàm .match() sẽ trả về null nếu không tìm thấy gì.
   // Chúng ta sẽ trả về mảng rỗng [] cho thống nhất.
   if (ketQua) {
     return ketQua;
   } else {
-    return [];
+    return 2;
   }
 };
