f9bf54895149ed17de551a0d878a214c33fc429f
Darren <nguyenletandat.contact@gmail.com>
2025-11-10 10:35:56 +0700

    config webhook

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,13 +1,13 @@
-import { sentMessageForUserByIdService } from "../services/chatbox.service";
+import { sentMessageForUserByIdService } from "../services/chatbox.service.js";
 
 export const webhookController = async (req, res) => {
-  res.status(200).send("EVENT_RECEIVED");
-  const data = req.body;
-  if (data.event_name === "user_send_text") {
-    const message = data.message.text;
-    const sender_id = data.sender.id;
-    sentMessageForUserByIdService(sender_id, "Phản hồi từ máy chủ của Nguyễn Lê Tấn Đạt")
-    console.log(message, sender_id);
-  }
-  return;
+  // res.status(200).send("EVENT_RECEIVED");
+  // const data = req.body;
+  // if (data.event_name === "user_send_text") {
+  //   const message = data.message.text;
+  //   const sender_id = data.sender.id;
+  //   sentMessageForUserByIdService(sender_id, "Phản hồi từ máy chủ của Nguyễn Lê Tấn Đạt")
+  //   console.log(message, sender_id);
+  // }
+  // return;
 };
