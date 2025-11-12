7636b4fbb4850372156f371a669809d08cd0e390
Nguyen Le Tan Dat <nguyenletandat.contact@gmail.com>
2025-11-12 16:06:29 +0700

    5584155984018191145

--- src/chats/analyze.service.js
+++ src/chats/analyze.service.js
@@ -107,7 +107,7 @@ export const analyzeUserMessageService = async (messageFromUser, UID) => {
 // (Sửa lại informationForwardingSynthesisService để dùng hàm sendZaloMessage)
 export const informationForwardingSynthesisService = async (dataCustomer) => {
   // UID của Lead/Quản lý
-  const LEAD_UID = "1591235795556991810";
+  const LEAD_UID = "5584155984018191145";
 
   try {
     const response = await sendZaloMessage(LEAD_UID, dataCustomer);
