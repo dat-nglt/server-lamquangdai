f18f17a726745568dbe7e6f9d98114234aa259c5
Nguyen Le Tan Dat <nguyenletandat.contact@gmail.com>
2025-11-14 15:52:25 +0700

    Google sheet config

--- worker.js
+++ worker.js
@@ -10,6 +10,7 @@ import {
     analyzeUserMessageService,
     informationForwardingSynthesisService,
 } from "./src/chats/analyze.service.js";
+import { appendJsonToSheet } from "./src/chats/googleSheet.js";
 
 const connection = {
     host: process.env.REDIS_HOST || "localhost",
