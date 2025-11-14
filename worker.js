60c6ad1882e4d81219605f09e55412229fab4457
Nguyen Le Tan Dat <nguyenletandat.contact@gmail.com>
2025-11-14 16:05:45 +0700

    Google sheet config

--- worker.js
+++ worker.js
@@ -10,7 +10,7 @@ import {
     analyzeUserMessageService,
     informationForwardingSynthesisService,
 } from "./src/chats/analyze.service.js";
-import { appendJsonToSheet } from "./src/chats/googleSheet.js";
+// import { appendJsonToSheet } from "./src/chats/googleSheet.js";
 
 const connection = {
     host: process.env.REDIS_HOST || "localhost",
@@ -108,6 +108,18 @@ const worker = new Worker(
                     }\n📞Vui lòng phân bổ liên hệ lại khách hàng ngay!`;
 
                     // BƯỚC 1: LƯU VÀO SHEET (Backup)
+                    // try {
+                    //     await appendJsonToSheet("data-m-1", jsonData);
+                    // } catch (sheetError) {
+                    //     // Lỗi nghiêm trọng: Không lưu được vào DB
+                    //     // Phải dừng lại và báo lỗi, KHÔNG gửi Zalo
+                    //     logger.error(
+                    //         `[Worker] LỖI NGHIÊM TRỌNG: Không thể ghi Sheet cho SĐT ${jsonData.soDienThoai}:`,
+                    //         sheetError.message
+                    //     );
+                    //     // Ném lỗi này ra để worker bên ngoài biết và retry
+                    //     throw sheetError;
+                    // }
 
                     try {
                         await informationForwardingSynthesisService(
@@ -124,19 +136,6 @@ const worker = new Worker(
                             `[Worker] Lỗi khi GỬI LEAD cho UID ${UID}:`,
                             leadError.message
                         );
-                    }
-
-                    try {
-                        await appendJsonToSheet("data-m-1", jsonData);
-                    } catch (sheetError) {
-                        // Lỗi nghiêm trọng: Không lưu được vào DB
-                        // Phải dừng lại và báo lỗi, KHÔNG gửi Zalo
-                        logger.error(
-                            `[Worker] LỖI NGHIÊM TRỌNG: Không thể ghi Sheet cho SĐT ${jsonData.soDienThoai}:`,
-                            sheetError.message
-                        );
-                        // Ném lỗi này ra để worker bên ngoài biết và retry
-                        throw sheetError;
                     }
                 }
             } else {
