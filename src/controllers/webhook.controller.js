77ab418d506393e3bfa5b966112d1df06cd6eebd
Nguyen Le Tan Dat <nguyenletandat.contact@gmail.com>
2025-11-20 15:51:09 +0700

    change MAX_REMINDERS to 1

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -2,7 +2,7 @@ import logger from "../utils/logger.js";
 import { zaloChatQueue } from "../chats/queue.service.js";
 
 const DEBOUNCE_DELAY = 20000; // 20 giây
-// const ALLOWED_UID = "7365147034329534561"; // UID được phép gửi tin nhắn
+const ALLOWED_UID = "7365147034329534561"; // UID được phép gửi tin nhắn
 
 export const handleZaloWebhook = async (req, res) => {
     try {
